ee086d3fb99278c86e13e5af8c352b69425741d4
arnau.merino <arnau.merino@csuc.cat>
2023-06-30 12:43:23 +0200

    Control de turnos y setWinner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -21,20 +21,58 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
     </div>
   )
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
-
   const [turn, setTurn] = useState(TURNS.X)
+  //null significa que no hay ganador, true ganador y false un empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si X o O ha nagado
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
 
   const updateBoard = (index) => {
+    //dont update position if it is not null
+    if (board[index]) return
+    //update board
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    //change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
   }
   return (
     <main className='board'>
